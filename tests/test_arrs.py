74b3457f85075ee9c8387ab1a3c9f094a7664064
Vladimir Afonin <vovan010698@mail.ru>
2023-12-19 22:53:38 +0300

    test1

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -4,8 +4,11 @@ from utils import arrs
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 3
     assert arrs.get([], 0, "test") == "test"
+    assert arrs.get([1, 2, 3], -1, "test") == "test"
 
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([1, 2, 3], -1) == [3]
+    assert arrs.my_slice([1, 2, 3], 0, -2) == [1]
