1b8745d05762079c4dc56d63b61b97bed4a3fabc
Vladimir Afonin <vovan010698@mail.ru>
2023-12-19 23:37:01 +0300

    issue2

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -2,7 +2,7 @@ from utils import arrs
 
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
+    assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([], 0, "test") == "test"
     assert arrs.get([1, 2, 3], -1, "test") == "test"
 
@@ -12,3 +12,5 @@ def test_slice():
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
     assert arrs.my_slice([1, 2, 3], -1) == [3]
     assert arrs.my_slice([1, 2, 3], 0, -2) == [1]
+    assert arrs.my_slice([1, 2, 3], None, 1) == [1]
+    assert arrs.my_slice([], 1) == []
